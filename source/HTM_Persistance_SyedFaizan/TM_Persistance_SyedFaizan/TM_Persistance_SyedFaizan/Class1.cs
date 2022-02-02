fa0fdd2ee35bb286c38c2f77b137594b392a0961
Syed Faizan Ul Hassan Zahidi <93147175+SyedFaizan18@users.noreply.github.com>
2022-02-02 21:16:19 +0100

    Classes for ser and deser created

---
+++ source/HTM_Persistance_SyedFaizan/TM_Persistance_SyedFaizan/TM_Persistance_SyedFaizan/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TM_Persistance_SyedFaizan
+{
+    internal class Class1
+    {
+        public void Serializer()
+        { 
+        }
+
+        public void Deserialize()
+        {
+        
+        
+        }
+
+    }
+}
---
+++ source/HTM_Persistance_SyedFaizan/TM_Persistance_SyedFaizan/TM_Persistance_SyedFaizan/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TM_Persistance_SyedFaizan;
+Console.WriteLine("Hello, World!");
+Class1 Class1 = new Class1();
+Class1.Serializer();
+Class1.Deserialize();
