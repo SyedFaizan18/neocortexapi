4a7f6313b1c2eec30fa40f82edf4143f03b36546
Syed Faizan Ul Hassan Zahidi <93147175+SyedFaizan18@users.noreply.github.com>
2022-02-06 21:44:28 +0100

    Objects of column,synapse,topoly,pool created

--- source/HTM_Persistance_SyedFaizan/TM_Persistance_SyedFaizan/TM_Persistance_SyedFaizan/Class1.cs
+++ source/HTM_Persistance_SyedFaizan/TM_Persistance_SyedFaizan/TM_Persistance_SyedFaizan/Class1.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using  NeoCortexApi;
+using NeoCortexApi.Entities;
 using System.Threading.Tasks;
 
 namespace TM_Persistance_SyedFaizan
@@ -9,7 +11,13 @@ namespace TM_Persistance_SyedFaizan
     internal class Class1
     {
         public void Serializer()
-        { 
+        {
+            SegmentActivity segment = new SegmentActivity();
+            Column column = new Column();
+            Synapse synapse = new Synapse();
+            Topology topology = new Topology();
+            Pool pool = new Pool();
+
         }
 
         public void Deserialize()
